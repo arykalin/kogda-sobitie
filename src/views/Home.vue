29066020af8fce02939bfee0b9de5a84b1c0b003
katerina <katerina.kamkova@mail.ru>
2021-09-25 13:16:15 +0300

    Add nice animation; to do: pretty picture + content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,205 +2,57 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>События</ion-title>
+        <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-list>
-        <ion-item>
-          <ion-label>
-            <h1>Чтения про всякое разное</h1>
-            <h2>Татка</h2>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">24 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>
-            <h1>Игра в 30ке</h1>
-            <ion-note>На среднем</ion-note>
-          </ion-label>
-          <!-- <ion-icon icon="heart"></ion-icon> -->
-          <ion-badge color="success" slot="end">25 сентября</ion-badge>
-        </ion-item>
-      </ion-list>
-
-      <ion-grid>
-        <ion-row>
-          <ion-col>
-            <div>Что</div>
-          </ion-col>
-          <ion-col>
-            <div>Где</div>
-          </ion-col>
-          <ion-col>
-            <div>Когда</div>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <div>Чтения</div>
-          </ion-col>
-          <ion-col>
-            <div>На среднем</div>
-          </ion-col>
-          <ion-col>
-            <div>24 сентября</div>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
-
-      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
-        <ion-fab-button @click="() => router.push('/new')">
-          <ion-icon :icon="add"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
-
-      <!-- <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
-        </p>
-      </div> -->
+      <accordion :list="list">
+        <template v-slot="sp">
+          <ion-item>{{ sp.item.id }} {{ sp.item.name }}</ion-item>
+        </template>
+      </accordion>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {
+  import {
   IonContent,
-  IonFab,
-  IonFabButton,
   IonHeader,
-  IonIcon,
   IonPage,
   IonTitle,
   IonToolbar,
-  IonCheckbox,
-  IonInput,
-  IonItem,
-  IonItemOption,
-  IonItemOptions,
-  IonItemSliding,
-  IonList,
-  IonLabel,
-  IonRadio,
-  IonToggle,
-} from "@ionic/vue";
-import { defineComponent } from "vue";
-import { add } from "ionicons/icons";
-import { useRouter } from "vue-router";
+  IonItem
+  } from "@ionic/vue";
+  import { defineComponent } from "vue";
+  import Accordion from "./components/Accordion.vue";
 
-export default defineComponent({
+  export default defineComponent({
   name: "Home",
   components: {
-    IonContent,
-    IonFab,
-    IonFabButton,
-    IonHeader,
-    IonIcon,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    IonCheckbox,
-    IonInput,
-    IonItem,
-    IonItemOption,
-    IonItemOptions,
-    IonItemSliding,
-    IonList,
-    IonLabel,
-    IonRadio,
-    IonToggle,
+  Accordion,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem
   },
-  setup() {
-    return {
-      router: useRouter(),
-      add,
-    };
+  data() {
+  return {
+  list: [
+  { id: 1, name: "a"},
+  { id: 2, name: "b"},
+  { id: 3, name: "c"},
+  { id: 4, name: "d"}
+  ]
+  };
   },
-});
+  methods: {}
+  });
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
+<style scoped="">
 
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
---
+++ src/views/components/Accordion.vue
@@ -0,0 +1,90 @@
+<template>
+  <div
+    v-for="listItem in displayList"
+    :key="listItem.id"
+    style="margin: 8px;  border:solid green 1px"
+  >
+    <div
+      :ref="'header-' + listItem.id"
+      class="ion-padding default-header"
+      @click="headerClicked(listItem)"
+    >
+      title {{ listItem.name }}
+    </div>
+    <transition name="fade">
+      <div
+        :ref="'body-' + listItem.id"
+        style="margin-top:8px;display:none; height: 100px;"
+        v-show="expandElement(listItem)"
+      >
+        <slot :item="listItem"></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Accordion",
+  // list of data to display
+  props: ["list"],
+  // data section of component
+  data() {
+    return {
+      displayList: this.list
+    };
+  },
+  methods: {
+    /**
+     * this function is called to determine if the element
+     * should be in the expanded mode or not
+     */
+    expandElement(listItem) {
+      const curE = this.$refs["body-" + listItem.id];
+      if (curE === undefined) return false;
+      if (curE.dataset.isExpanded === "true") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    /**
+     * this iterates through all of the elements in the list
+     * and set data attribute isExpanded appropriately based on
+     * this listItem that was clicked
+     */
+    headerClicked(listItem) {
+      this.displayList.map(function(e) {
+        const curE = this.$refs["body-" + e.id];
+        if (e === listItem) {
+          if (curE.dataset.isExpanded === "true") {
+            curE.setAttribute("data-is-expanded", false);
+          } else {
+            curE.setAttribute("data-is-expanded", true);
+          }
+        } else {
+          curE.setAttribute("data-is-expanded", false);
+        }
+      }, this);
+      this.displayList = [...this.displayList];
+    }
+  }
+};
+</script>
+
+<style  scoped>
+.default-header {
+  background-color: gray;
+  margin: 2px;
+  text-transform: uppercase;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: height 0.3s ease-in-out;
+  overflow: hidden;
+}
+.fade-enter-from,
+.fade-leave-to {
+  height: 0px !important;
+}
+</style>

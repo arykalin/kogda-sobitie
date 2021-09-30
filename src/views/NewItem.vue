34d58eed0c025b9488c6a113b753031182998adc
katerina <katerina.kamkova@mail.ru>
2021-09-30 18:10:09 +0300

    create 'fill form' for adding new event, haven`t solved all bugs yet

--- src/views/NewItem.vue
+++ src/views/NewItem.vue
@@ -5,12 +5,81 @@
         <ion-buttons slot="start">
           <ion-back-button default-href="/home"></ion-back-button>
         </ion-buttons>
-        <ion-title>New Item</ion-title>
+        <ion-title>Создать событие</ion-title>
       </ion-toolbar>
     </ion-header>
-      <ion-content ref="content">
-    <ion-button @click="scrollToBottom">Scroll to Bottom</ion-button>
-  </ion-content>
+    <ion-content ref="content">
+      <ion-item>
+        <ion-input
+          v-model="title"
+          placeholder="Название события"
+          required="true"
+        ></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-textarea
+          v-model="description"
+          placeholder="Описание"
+        ></ion-textarea>
+      </ion-item>
+
+      <ion-item>
+        <ion-input v-model="org" placeholder="Ведущий"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input v-model="place" placeholder="Место проведения"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input
+          v-model="date"
+          placeholder="Дата проведения"
+          type="date"
+        ></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input v-model="duration" placeholder="Длительность"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input v-model="target" placeholder="Для кого"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input
+          v-model="amount"
+          placeholder="Сколько людей"
+          type="number"
+        ></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-input v-model="link" placeholder="Ссылка" type="url"></ion-input>
+      </ion-item>
+
+      <ion-button
+        @click="
+          eventCreated(
+            title,
+            description,
+            org,
+            place,
+            date,
+            duration,
+            target,
+            amount,
+            link
+          )
+        "
+        expand="block"
+        fill="outline"
+        color="medium"
+        >Сохранить</ion-button
+      >
+    </ion-content>
   </ion-page>
 </template>
 
@@ -22,13 +91,13 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
-} from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
-
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+import axios from 'axios';
 
 export default defineComponent({
-  name: 'NewItem',
+  name: "NewItem",
   components: {
     IonBackButton,
     IonButtons,
@@ -36,14 +105,50 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
   },
-    setup() {
-      const content = ref();
-      const scrollToBottom = () => {
-        content.value.$el.scrollToBottom(300);
-      }
+  methods: {
+    eventCreated(
+      title,
+      description,
+      org,
+      place,
+      date,
+      duration,
+      target,
+      amount,
+      link
+    ) {
+      const axios = require('axios');
+      const data = JSON.stringify({
+        "date": date,
+        "title": title,
+        "duration": duration,
+        "link": link,
+        "who_manages": org,
+        "for_whom": target,
+        "where": place,
+        "description": description,
+        "wanting_people": amount
+      });
 
-      return { content, scrollToBottom }
-}});
+      const config = {
+        method: "post",
+        url: "http://localhost:8080/event",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+});
 </script>

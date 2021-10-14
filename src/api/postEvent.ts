ba843f1460b8a42634309e14f9c84be493aacd8e
arykalin <arykalin@gmail.com>
2021-10-14 18:47:07 +0300

    Setting toke header for post event

--- src/api/postEvent.ts
+++ src/api/postEvent.ts
@@ -4,10 +4,14 @@ import Event from "@/types/Event";
 const END_POINT = '/event'
 
 const postEvent = (event: Event): Promise<any> => {
-    console.log('constructed event in post: ' + event.description)
+    const token = localStorage.getItem('token')
+    const headers = {
+        "Token": token
+    };
+    console.log('using token ' + token)
     const obj: MyObj = JSON.stringify(event)
     console.log('Posting event: ' + obj)
-    return httpClient.post(`${END_POINT}`, obj)
+    return httpClient.post(`${END_POINT}`, obj, { headers })
 }
 
 export {postEvent}

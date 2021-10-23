539cec1e44c0d3680267de0edd2f2f6b7458f62c
arykalin <arykalin@gmail.com>
2021-10-23 13:56:01 +0300

    fixing get events

--- src/api/getEvents.ts
+++ src/api/getEvents.ts
@@ -1,31 +1,32 @@
 import httpClient from './httpClient'
 import Event from "@/types/Event";
+
 const END_POINT = '/events'
 
 const getEvents = (): Event[] => {
-  let response = httpClient.get(`${END_POINT}`);
-  let answer;
-  httpClient.get(`${END_POINT}`).then((response) => {
-    answer = response.data.events.map(event => {
-      const newEvent: Event = {
-        title: event.title,
-        description: event.description,
-        org: event.org,
-        where: event.where,
-        date: new Date(event.date),
-        duration: event.duration,
-        target: event.target,
-        amount: event.amount,
-        link: event.link,
-      };
-    })
-    console.log("ccc");
-    console.log(answer);
-    //return answer;
-  });  
-  console.log("bbb");
-  console.log(answer);
-  return answer;
+    let answer!: Event[];
+    httpClient.get(`${END_POINT}`)
+        .then((response) => {
+            console.log("got response: ", response)
+            answer = response.data.events.map(event => {
+                const newEvent: Event = {
+                    title: event.title,
+                    description: event.description,
+                    org: event.org,
+                    where: event.where,
+                    date: new Date(event.date),
+                    duration: event.duration,
+                    target: event.target,
+                    amount: event.amount,
+                    link: event.link,
+                };
+            })
+            console.log("formed answer: ", answer);
+        })
+        .catch((error) => {
+            console.error("error getting events", error);
+        })
+    return answer
 }
 
-export { getEvents }
+export {getEvents}
--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = "http://95.216.158.138:80"
+const baseURL = "http://127.0.0.1:8080"
 
 const httpClient = axios.create({
   baseURL,

9ddeb6141b6804c8e7372c1abd98ff8fb3bc5754
arykalin <arykalin@gmail.com>
2021-10-28 17:58:44 +0300

    fixing get events

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -54,6 +54,7 @@ export default defineComponent({
     events() {
       // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
       // @ts-ignore
+
       return this.store.getters['event/events'] as Event[]
     },
   },

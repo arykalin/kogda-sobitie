bec3d6a8ffd6e751edf45a676ffa3ec536aebc5c
katerina-kamkova <katerina.kamkova@mail.ru>
2021-12-09 18:14:44 +0300

    fixed api

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseURL = "http://127.0.0.1:8080"
-const baseURL = "https://95.216.158.138:80"
+const baseURL = "http://95.216.158.138:80"
 
 const httpClient = axios.create({
   baseURL,
